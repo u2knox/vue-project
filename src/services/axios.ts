47a24dab6950ad77f4e16c226a676a9b08c2dd5a
u2knox <u2knox@gmail.com>
2023-07-18 14:35:41 +0600

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,3 +1,13 @@
 import axios from "axios";
 
+const access_token = "fdsafadsfdsfsadf"
+
+axios.interceptors.request.use(function (config) {
+  config.headers.Authorization = `Bearer ${access_token}`;
+  return config;
+}, function (error) {
+  // Сделайте что-нибудь с ошибкой запроса
+  return Promise.reject(error);
+})
+
 export default axios;
